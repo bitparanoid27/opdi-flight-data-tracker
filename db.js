9f3124ec91e7a3e2c59dbfce9f20c477dc5036d4
Sourabh <sourabhgaikwad113@gmail.com>
2026-05-05 05:56:26 +0200

    First commit

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-// Push the code to the github repo. 
+// Push the code to the github repo.
+//
